645529792bf737a2eb27af5c0ba6c5d0d36d504f
huddah01 <115144711+huddah01@users.noreply.github.com>
2022-10-06 01:34:47 -0700

    Create 001TwoSum.cpp

---
+++ 001TwoSum.cpp
@@ -0,0 +1,39 @@
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        /**
+        // Initialize vector
+        vector<int> sum;
+        // Nested for loop to go through everything
+        for(int i = 0; i < nums.size() - 1; i++) {
+            for(int j = i + 1; j < nums.size(); j++) {
+                if(nums[i] + nums[j] == target) {
+                    sum.insert(sum.begin(), i);
+                    sum.insert(sum.begin(), j);
+                }
+            }
+        }
+        // Return sum
+        return sum;
+        */
+        // Initialize hash map
+        unordered_map<int, int> map;
+        // Initialize vector
+        vector<int> sum;
+        
+        // Insert elements
+        for(int i = 0; i < nums.size(); i++) {
+            map[nums[i]] = i;
+        }
+        // Check if complement exists and return
+        for(int i = 0; i < nums.size(); i++) {
+            int complement = target - nums[i];
+            if(map.find(complement) != map.end() && map[complement] != i) {
+                sum.insert(sum.begin(), map[complement]);
+                sum.insert(sum.begin(), i);
+                return sum;
+            }
+            //map[nums[i]] = i;
+        }
+        return sum;
+    }
+};
